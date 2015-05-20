0397a76d527b5e2f115e16b1a36912b0f79d7406
Arne Morten Kvarving <cptspiff@gmail.com>
2015-05-20 15:49:44 +0200

    changed: move OnPrepareFileItems to music base class

--- xbmc/music/windows/GUIWindowMusicBase.cpp
+++ xbmc/music/windows/GUIWindowMusicBase.cpp
@@ -1375,3 +1375,11 @@ void CGUIWindowMusicBase::OnRemoveSource(int iItem)
     g_infoManager.ResetLibraryBools();
   }
 }
+
+void CGUIWindowMusicBase::OnPrepareFileItems(CFileItemList &items)
+{
+  CGUIMediaWindow::OnPrepareFileItems(items);
+
+  if (!items.IsMusicDb())
+    RetrieveMusicInfo();
+}
