PhenomG4/kodi-cmake

changed: make CBuiltins a singleton

xbmc/addons/GUIDialogAddonInfo.cpp
@@ -193,7 +193,7 @@ void CGUIDialogAddonInfo::OnLaunch()
     return;
 
   Close();
-  CBuiltins::Execute("RunAddon(" + m_localAddon->ID() + ")");
+  CBuiltins::Get().Execute("RunAddon(" + m_localAddon->ID() + ")");
 }
 
 bool CGUIDialogAddonInfo::PromptIfDependency(int heading, int line2)

xbmc/input/ButtonTranslator.cpp
@@ -1324,7 +1324,7 @@ bool CButtonTranslator::TranslateActionString(const char *szAction, int &action)
   action = ACTION_NONE;
   std::string strAction = szAction;
   StringUtils::ToLower(strAction);
-  if (CBuiltins::HasCommand(strAction)) 
+  if (CBuiltins::Get().HasCommand(strAction))
     action = ACTION_BUILT_IN_FUNCTION;
 
   for (unsigned int index=0;index < ARRAY_SIZE(actions);++index)

xbmc/utils/RssManager.cpp
@@ -76,7 +76,7 @@ void CRssManager::OnSettingAction(const CSetting *setting)
         return;
       CAddonInstaller::Get().Install("script.rss.editor", true, "", false);
     }
-    CBuiltins::Execute("RunScript(script.rss.editor)");
+    CBuiltins::Get().Execute("RunScript(script.rss.editor)");
   }
 }
 

notspiff/xbmc

xbmc/addons/GUIDialogAddonInfo.cpp
@@ -214,7 +214,7 @@ void CGUIDialogAddonInfo::OnLaunch()
     return;
 
   Close();
-  CBuiltins::Execute("RunAddon(" + m_localAddon->ID() + ")");
+  CBuiltins::GetInstance().Execute("RunAddon(" + m_localAddon->ID() + ")");
 }
 
 bool CGUIDialogAddonInfo::PromptIfDependency(int heading, int line2)

xbmc/input/ButtonTranslator.cpp
@@ -1364,7 +1364,7 @@ bool CButtonTranslator::TranslateActionString(const char *szAction, int &action)
   action = ACTION_NONE;
   std::string strAction = szAction;
   StringUtils::ToLower(strAction);
-  if (CBuiltins::HasCommand(strAction)) 
+  if (CBuiltins::GetInstance().HasCommand(strAction))
     action = ACTION_BUILT_IN_FUNCTION;
 
   for (unsigned int index=0;index < ARRAY_SIZE(actions);++index)

xbmc/utils/RssManager.cpp
@@ -84,7 +84,7 @@ void CRssManager::OnSettingAction(const CSetting *setting)
       }
       CAddonInstaller::GetInstance().InstallOrUpdate("script.rss.editor", "", false);
     }
-    CBuiltins::Execute("RunScript(script.rss.editor)");
+    CBuiltins::GetInstance().Execute("RunScript(script.rss.editor)");
   }
 }
 