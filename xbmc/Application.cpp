3f2226449305605db855f19dc4c4a8bf566c2251
notspiff <cptspiff@gmail.com>
2018-04-09 19:00:00 +0200

    changed: remove internal sftp support

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -111,9 +111,6 @@
 #ifdef HAS_FILESYSTEM_NFS
 #include "filesystem/NFSFile.h"
 #endif
-#ifdef HAS_FILESYSTEM_SFTP
-#include "filesystem/SFTPFile.h"
-#endif
 #include "PartyModeManager.h"
 #include "network/ZeroconfBrowser.h"
 #ifndef TARGET_POSIX
@@ -2870,10 +2867,6 @@ void CApplication::Stop(int exitCode)
     }
 #endif
 
-#ifdef HAS_FILESYSTEM_SFTP
-    CSFTPSessionManager::DisconnectAllSessions();
-#endif
-
     for (const auto& vfsAddon : CServiceBroker::GetVFSAddonCache().GetAddonInstances())
       vfsAddon->DisconnectAll();
 
@@ -4393,10 +4386,6 @@ void CApplication::ProcessSlow()
   gNfsConnection.CheckIfIdle();
 #endif
 
-#ifdef HAS_FILESYSTEM_SFTP
-  CSFTPSessionManager::ClearOutIdleSessions();
-#endif
-
   for (const auto& vfsAddon : CServiceBroker::GetVFSAddonCache().GetAddonInstances())
     vfsAddon->ClearOutIdle();
 
@@ -5045,10 +5034,6 @@ void CApplication::CloseNetworkShares()
 
 #ifdef HAS_FILESYSTEM_NFS
   gNfsConnection.Deinit();
-#endif
-
-#ifdef HAS_FILESYSTEM_SFTP
-  CSFTPSessionManager::DisconnectAllSessions();
 #endif
 
   for (const auto& vfsAddon : CServiceBroker::GetVFSAddonCache().GetAddonInstances())
